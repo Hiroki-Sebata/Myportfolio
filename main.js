76a111d1f5cf40beaa9fff9c691a59462fde7e78
Hiroki_S <sebasuta@gmail.com>
2021-01-31 12:20:35 +0900

    first

---
+++ main.js
@@ -0,0 +1,81 @@
+const slideWidth = 800;
+
+let currentSlide = 0;
+
+let sumSlide;
+
+const showSlide = (index)=>{
+    if(index=== 0){
+        $('.slide-control-prev').hide();
+
+    }else{
+        $('.slide-control-prev').show();
+    }
+
+    if(index===numSlide-1){
+        $('.slide-control-next').hide();
+    }else{
+        $('.slide-control-next').show();
+    }
+
+    $('.slide')
+    .stop()
+    .animate(
+        {
+            left: `${-slideWidth * index}px`,
+        },
+        800, /** this meeans how long the duration will last */
+    );
+};
+
+$('.slide-control-prev').on('click',(e)=>{
+    e.preventDefault();
+
+    currentSlide -= 1;
+    showSlide(currentSlide);
+});
+
+$('.slide-control-next').on('click',(e)=>{
+    e.preventDefault();
+
+    currentSlide += 1;
+    showSlide(currentSlide);
+});
+
+numSlide = $('.slide > li').length;
+
+showSlide(currentSlide);
+
+const socialButton =()=>{
+    if($(window).scrollTop() >=300){
+        $('.socialize').fadeIn();
+    }else {
+        $('.socialize').fadeOut();
+    }
+};
+
+$(window).on('scroll',socialButton);
+
+socialButton();
+
+const backToTop =()=>{
+    if($(window).scrollTop() >= 300){
+        $('.back-to-top').fadeIn();
+    }else{
+        $('.back-to-top').fadeOut();
+    }
+};
+
+$(window).on('scroll',backToTop);
+
+$('.back-to-top').on('click', (e)=>{
+    e.preventDefault();
+    $('html,body').animate(
+        {
+            scrollTop: 0
+        }, 600,
+        );
+});
+
+backToTop();
+
